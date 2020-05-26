9d303c59d5cfe4dd93c6d51d22d1ad606099e128
lol3rrr <s.loler03@gmail.com>
2020-05-26 17:13:47 +0200

    Fixed small segfault problem

--- checkConnection.go
+++ checkConnection.go
@@ -4,9 +4,11 @@ import "database/sql"
 
 // ObtainConnection creates a new connection if the old one expired, otherwise does nothing
 func (d *DB) ObtainConnection() (*sql.DB, error) {
-	err := d.SQL.Ping()
-	if err == nil {
-		return d.SQL, nil
+
+	if d.SQL != nil {
+		if err := d.SQL.Ping(); err == nil {
+			return d.SQL, nil
+		}
 	}
 
 	if err := d.connect(); err != nil {
