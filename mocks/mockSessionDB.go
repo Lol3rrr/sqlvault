e574e7d33840c8ef614518f4fbdd0dabec677e70
lol3rrr <s.loler03@gmail.com>
2020-09-05 12:55:56 +0200

    Fixed small problem

--- mocks/mockSessionDB.go
+++ mocks/mockSessionDB.go
@@ -7,10 +7,10 @@ import (
 // MockSessionDB is used as a simple mock implementation for testing
 // This version does call the function parameter
 type MockSessionDB struct {
-	mockDB *MockDB
+	MockDB *MockDB
 }
 
 // WithRetry is needed to comply with the interface
 func (m *MockSessionDB) WithRetry(call func(con sqlvault.DB) error) error {
-	return call(m.mockDB)
+	return call(m.MockDB)
 }
